a35297e0d9cb328ea28d81841232bc633f2c8360
farahwalid <45386895+farahwalid@users.noreply.github.com>
2020-04-18 00:53:09 +0200

    Add files via upload

---
+++ model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+
+# Import necessary modules
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+# Keras specific
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+from keras.layers import Dropout
